c64c13ca23064ac3d1cd95425f3a8393c6c9142a
David Cavazos <davido262@gmail.com>
2013-09-20 09:58:02 -0500

    Added SDL2 graphics and audio

--- main.cpp
+++ main.cpp
@@ -1,119 +1,53 @@
 #include <cstdio>
 #include <cstdlib>
-#include <SDL/SDL.h>
+#include <string>
 #include "config.hpp"
+#include "device.hpp"
+#include "game.hpp"
+
+using namespace std;
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-const int SCREEN_DEPTH = 32;
-
-const Uint32 SDL_INIT_FLAGS = SDL_INIT_VIDEO;
-const Uint32 SDL_VIDEO_FLAGS = SDL_HWSURFACE | SDL_ANYFORMAT | SDL_DOUBLEBUF |
-                               SDL_RESIZABLE;
 
 const int FRAME_CAP = 60;
 const int FRAME_CAP_MS = 1000 / FRAME_CAP;
 
+const string MATERIALS_FILENAME = "assets/materials";
+const string MAP_FILENAME = "assets/map1";
+const string CHARACTERS_FILENAME = "assets/characters";
+
 int main(int, char**) {
   printf("Dungeon Master v%d.%d\n", VERSION_MAJOR, VERSION_MINOR);
-  puts("Designed and programmed by: David Cavazos\n");
+  puts("Designed and programmed by: David Cavazos");
+  puts("Music from: Dwarf Fortress");
 
-  SDL_Surface* screen;
-  int screen_w = SCREEN_WIDTH;
-  int screen_h = SCREEN_HEIGHT;
-  int screen_d = SCREEN_DEPTH;
-
-  puts("Initializing SDL");
-  if (SDL_Init(SDL_INIT_FLAGS) != 0) {
-    fprintf(stderr, "Error initializing SDL: flags=0x%8x\n", SDL_INIT_FLAGS);
-    exit(EXIT_FAILURE);
-  }
-  screen = SDL_SetVideoMode(screen_w, screen_h, screen_d, SDL_VIDEO_FLAGS);
-  if (screen == NULL) {
-    fprintf(stderr, "Error setting video mode: %dx%dx%d, flags=0x%8x\n",
-            screen_w, screen_h, screen_d, SDL_VIDEO_FLAGS);
-    SDL_Quit();
-    exit(EXIT_FAILURE);
-  }
+  Device dev(SCREEN_WIDTH, SCREEN_HEIGHT);
+  Game game(dev, MATERIALS_FILENAME, MAP_FILENAME, CHARACTERS_FILENAME);
 
   puts("Running game loop");
-  SDL_Event event;
   char buffer[64];
-  Uint32 start_time;
-  Uint32 delta_time;
+  unsigned int start_time;
+  unsigned int delta_time;
   bool is_running = true;
   while (is_running) {
-    start_time = SDL_GetTicks();
+    start_time = dev.get_time();
+    dev.clear_screen();
 
-    // process events
-    while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-      case SDL_QUIT:
-        is_running = false;
-        break;
-
-      case SDL_KEYDOWN:
-        // event.key.keysym.sym
-        break;
-
-      case SDL_KEYUP:
-        switch (event.key.keysym.sym) {
-        case SDLK_ESCAPE:
-          is_running = false;
-          break;
-        case SDLK_SPACE:
-          break;
-        case SDLK_w:
-          break;
-        case SDLK_a:
-          break;
-        case SDLK_s:
-          break;
-        case SDLK_d:
-          break;
-        case SDLK_j:
-          break;
-        case SDLK_k:
-          break;
-        default:
-          break;
-        }
-        break;
-
-      case SDL_MOUSEBUTTONDOWN:
-        // event.button.button
-        break;
-
-      case SDL_MOUSEBUTTONUP:
-        // event.button.button
-        break;
-
-      case SDL_MOUSEMOTION:
-        // event.motion.x
-        // event.motion.y
-        // event.motion.xrel
-        // event.motion.yrel
-        break;
-
-      default:
-        // ignore other events
-        break;
-      }
-    }
+    is_running = dev.process_events(game);
 
     // draw to screen
-    SDL_Flip(screen);
+    dev.draw_game(game);
 
     // control framerate
-    delta_time = SDL_GetTicks() - start_time;
-    sprintf(buffer, "Dungeon Master - %02u ms", delta_time);
-    SDL_WM_SetCaption(buffer, buffer);
+    delta_time = dev.get_time() - start_time;
+    sprintf(buffer, "Dungeon Master by David Cavazos - %02u ms", delta_time);
+    dev.set_title(buffer);
+
+    dev.render();
     if (FRAME_CAP_MS > delta_time) {
-      SDL_Delay(FRAME_CAP_MS - delta_time);
+      dev.sleep(FRAME_CAP_MS - delta_time);
     }
   }
-
-  puts("Quitting SDL");
-  SDL_Quit();
   return EXIT_SUCCESS;
 }
